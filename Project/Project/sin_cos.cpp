4cee139f6d557af3b28dc0b15eb9d1fcf5aa3e11
konata <amausakonata1932@gmail.com>
2019-04-19 23:33:08 +0900

    no message

--- Project/Project/sin_cos.cpp
+++ Project/Project/sin_cos.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string>
+#define _USE_MATH_DEFINES
 #include <math.h>
 #if (_DEBUG)
 #define _CRTDBG_MAP_ALLOC
@@ -7,13 +8,18 @@
 #include <crtdbg.h>
 #define new ::new(_NORMAL_BLOCK, __FILE__, __LINE__)
 #endif
-
+float Toradian(float angle)
+{
+	return (angle * M_PI) / 180.0f;
+}
 int main()
 {
 	unsigned int time = 0;
 	while (time <= 360)
 	{
-		std::cout << sin(time) << ":" << cos(time) << "\n";
+		//std::cout <<time << ":" << (sin(time) + 1) / 2 << ":" << (cos(time) + 1) / 2 << "\n";
+		printf("%d:%.5f:%.5f\n", time, (sin(Toradian(time + 270)) + 1) / 2, (cos(Toradian(time)) + 1) / 2);
+		//printf("%d:%.5f:%.5f\n", time, sin(Toradian(time)), cos(Toradian(time)));
 		time++;
 	}
 	system("pause");
